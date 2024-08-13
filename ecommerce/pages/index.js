d14ff152e58cf1fc130ee1e5e075729e49e2b252
jesusmacq <jesus.quezada99@gmail.com>
2024-08-13 15:17:25 -0600

    ✨ importamos el componente

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,9 +1,11 @@
 //import Head from 'next/head'
 import Layout from '../components/Layout'
+import Products from '../components/Products'
 export default function Home() {
   return (
     <>
       <Layout>
+        <Products />
       </Layout>
     </> 
   )
