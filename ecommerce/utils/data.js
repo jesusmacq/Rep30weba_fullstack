e2e4aa8c2c7aed616ffe065f8c0a911601b92e58
jesusmacq <jesus.quezada99@gmail.com>
2024-08-13 14:30:13 -0600

    creamos la data de products

---
+++ ecommerce/utils/data.js
@@ -0,0 +1,60 @@
+export const data = {
+    products: [
+        {
+            id: 1,
+            title: 'Product 1',
+            description: 'This is the first product',
+            price: 100,
+            image: '/sopa.jpg'   
+        },
+        {
+            id: 2,
+            title: 'Product 2',
+            description: 'This is the second product',
+            price: 200,
+            image: '/orios,jpg'   
+        },
+        {
+            id: 3,
+            title: 'Product 3',
+            description: 'This is the Third product',
+            price: 300,
+            image: '/mermelada,jpg'   
+        },
+        {
+            id: 4,
+            title: 'Product 4',
+            description: 'This is the fourh product',
+            price: 400,
+            image: '/elotes.jpg'   
+        },
+        {
+            id: 5,
+            title: 'Product 5',
+            description: 'This is the fifth product',
+            price: 500,
+            image: '/pepsi.jpg'   
+        },
+        {
+            id: 6,
+            title: 'Product 6',
+            description: 'This is the sixth product',
+            price: 600,
+            image: '/acetunas.jpg'   
+        },
+        {
+            id: 7,
+            title: 'Product 7',
+            description: 'This is the seventh product',
+            price: 700,
+            image: '/verduras'   
+        },
+        {
+            id: 8,
+            title: 'Product 8',
+            description: 'This is the eigth product',
+            price: 800,
+            image: '/nutela'   
+        }                           
+    ]
+}
