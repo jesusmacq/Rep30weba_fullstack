a4de492e585648bcaaad3c467724523fb0c01e4a
jesusmacq <jesus.quezada99@gmail.com>
2024-08-15 14:49:05 -0600

    Quitamos el componente Layout

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,12 +1,10 @@
 //import Head from 'next/head'
-import Layout from '../components/Layout'
+
 import Products from '../components/Products'
 export default function Home() {
   return (
     <>
-      <Layout>
-        <Products />
-      </Layout>
+       <Products />
     </> 
   )
 }
