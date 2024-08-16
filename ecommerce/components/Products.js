db7b3e50959b238423b59016d37ea06474d98b83
jesusmacq <jesus.quezada99@gmail.com>
2024-08-16 08:36:41 -0600

    Correccion en el archivo Products.js

--- ecommerce/components/Products.js
+++ ecommerce/components/Products.js
@@ -1,38 +1,39 @@
-import React from 'react' //importamos el componente react
-import { data } from '../utils/data' //importamos el archivo data.js
-import { Image } from '@chakra-ui/react' //importamos el componente Image de chakra ui
-import { Grid, GridItem, Card, Box } from '@chakra-ui/react' //importamos el componente Grid de chakra ui
-import Link from 'next/link' //importamos el componente Link de next
+import React from 'react'
+import { data } from '../utils/data'
+import { Image } from '@chakra-ui/react'
+import { Grid, GridItem, Card, Box } from '@chakra-ui/react'
+import Link from 'next/link'
 
-
-const Products = () => { 
+const Products = () => {
   return (
     <div>
-        <Grid templateColumns={{base: "1fr", lg: "repeat(4, 1fr)"}} gap={6}>
-          {data.products.map((products) => (
-            <GridItem key={products.id} colSpan={1}>
-              <Card>
+      <Grid templateColumns={{ base: "1fr", lg: "repeat(4, 1fr)" }} gap={6}>
+        {data.products.map((products) => (
+          <GridItem key={products.id} colSpan={1}>
+            <Card>
               <Link href={`/product/${products.id}`}>
-              <Image 
+                <Image
                   src={`/images${products.image}`}
                   height={230}
                   width={230}
-                  style={{objectFit: "cover" , height:"230px"}}
+                  style={{ objectFit: "cover", height: "230px" }}
                   alt={products.title}
-              />  
-              <Box py={1} px={3}>
-                <h3>{products.title}</h3>
-                <div>{products.description}</div>
-                <div>{products.price}</div>
-              </Box>
+                />
+                <Box py={1} px={3} >
+                  <h3>{products.title}</h3>
+                  <div>{products.description}</div>
+                  <div>{products.price}</div>
+                </Box>
               </Link>
-              </Card>
-            </GridItem>
-
-           ))}
-        </Grid>
+            </Card>
+          </GridItem>
+        ))}
+      </Grid>
     </div>
   )
 }
+
+
+
 
 export default Products
